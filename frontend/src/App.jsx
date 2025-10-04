1d86cc2a68a5ad5e0f4f1ef72a0b2ed8f494d82c
Jonas Dias Cândido <jonasdcandido210@gmail.com>
2025-10-04 16:05:11 -0300

    Adicionar páginas de redirecionar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,50 @@ function App() {
   return (
     <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login/medico" element={<DoctorPage />} />
+        <Route path="/login/paciente" element={<PacientPage />} />
+        <Route path="/testpaciente" />
+        <Route path="/testmedico" element={<DoctorPage />}  />
+        <Route path="/testmedico" element={<PacientPage />} />
     </Routes>
   );
+}
+
+const LoginPage = () => {
+    return (
+        <section className="max-w-lg m-auto mt-60">
+            <article className="flex flex-col md:flex-row  gap-8">
+                <Link to="/login/paciente" className="bg-white p-20 rounded-lg text-center text-2xl">
+                    <img src="" />
+                    PACIENTE
+                </Link>
+                <Link to="/login/medico" className="bg-white p-20 rounded-lg text-center text-2xl">
+                    <img src=""/>
+                    MÉDICO
+                </Link>
+            </article>
+        </section>
+    )
+}
+
+const DoctorPage = () => {
+    return (
+        <NavBar />
+    )
+}
+
+const PacientPage = () => {
+    return (
+        <NavBar />
+    )
+}
+
+const NavBar = () => {
+    return (
+        <nav>
+        </nav>
+    )
 }
 
 export default App;

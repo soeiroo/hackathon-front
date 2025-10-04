5e2610df14715d945e3a1bf32caf80f3497b4537
Jonas Dias Cândido <jonasdcandido210@gmail.com>
2025-10-04 14:50:51 -0300

    feat: Responsividade alterada

--- frontend/src/components/HomeHeader.jsx
+++ frontend/src/components/HomeHeader.jsx
@@ -9,16 +9,6 @@ function HomeHeader() {
           <img src='hcs-logo.png' alt='Healthcare Systems Logo' />
         </div>
         <ul>
-          <li>Pesquisa</li>
-          <li>Educação & Treinamento</li>
-          <li>Contato</li>
-          <li>Ajuda</li>
-          <div className='SearchSection'>
-            <form>
-              <input type="text" placeholder="Pesquisar..." />
-              <button type="submit">Buscar</button>
-            </form>
-          </div>
           <div className='PortalSection'>
             <Link to="/atendente/login" className='PortalButton'>
               Portal
@@ -26,14 +16,6 @@ function HomeHeader() {
           </div>
         </ul>
       </div>
-      <nav>
-        <ul className='NavLinks'>
-          <li><Link to="/">Agendar Consulta</Link></li>
-          <li><Link to="/about">Sobre Câncer & Tratamento</Link></li>
-          <li><Link to="/services">Serviços</Link></li>
-          <li><Link to="/donate">Doar Agora</Link></li>
-        </ul>
-      </nav>
       <div className='HeaderLine'></div>
     </header>
   );
